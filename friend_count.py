4b096da4d01a39de21f95fb0f940d99e1dacc53a
Alastair Paragas <alastairparagas@gmail.com>
2016-11-08 17:44:15 -0500

    Fixed spacing

--- friend_count.py
+++ friend_count.py
@@ -5,15 +5,10 @@ mr = MapReduce.MapReduce()
 
 
 def mapper(friendTuple):
-    
     personName = friendTuple[0];
-    
     mr.emit_intermediate(personName, 1);
     
-
 def reducer(personName, personCardinalities):
-    
     mr.emit((personName, sum(personCardinalities)))
-    
     
 mr.execute(open(sys.argv[1]), mapper, reducer)
